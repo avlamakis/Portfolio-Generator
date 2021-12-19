a823a39b0a65450ad395646ba51474ccd0eddb43
alexvlamakis <alexvlamakis@gmail.com>
2021-12-18 18:14:42 -0700

    code clean up

--- test/sample-answers.js
+++ test/sample-answers.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: 'Lernantino',
-  github: 'lernantino',
+  name: 'Alex',
+  github: 'avlamakis',
   confirmAbout: true,
   about:
     'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus. Proin pretium, velit eget ultricies pulvinar, sem ex eleifend elit, nec pretium ante magna ac nibh. Nulla pulvinar turpis non magna porttitor porttitor. Morbi pellentesque sed nibh eu gravida. Fusce rhoncus, turpis non ullamcorper tristique, enim nulla vehicula ligula, nec gravida augue elit nec mauris. Donec imperdiet justo et auctor vehicula. Ut eget dapibus metus. Nulla facilisi. Integer in porttitor eros.',
@@ -10,7 +10,7 @@ module.exports = {
       description:
         'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
       languages: ['HTML', 'CSS'],
-      link: 'https://github.com/lernantino/run-buddy',
+      link: 'https://github.com/avlamakis/run-buddy',
       feature: true,
       confirmAddProject: true
     },
@@ -19,7 +19,7 @@ module.exports = {
       description:
         'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
       languages: ['JavaScript', 'HTML', 'CSS'],
-      link: 'https://github.com/lernantino/taskinator',
+      link: 'https://github.com/avlamakis/taskinator',
       feature: true,
       confirmAddProject: true
     },
@@ -28,7 +28,7 @@ module.exports = {
       description:
         'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
       languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
-      link: 'https://github.com/lernantino/taskmaster-pro',
+      link: 'https://github.com/avlamakis/taskmaster-pro',
       feature: false,
       confirmAddProject: true
     },
@@ -37,7 +37,7 @@ module.exports = {
       description:
         'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
       languages: ['JavaScript'],
-      link: 'https://github.com/lernantino/robot-gladiators',
+      link: 'https://github.com/avlamakis/robot-gladiators',
       feature: false,
       confirmAddProject: false
     }
